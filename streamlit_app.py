033ab156192bcea8b32e22856e4ecac61757be58
shermank216 <shermank216@gmail.com>
2026-09-07 15:52:34 -0400

    import requests v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 # New section to deploy smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
